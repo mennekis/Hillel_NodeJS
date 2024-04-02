b8cdd535b089a6d3b3c8dbc1af220897861a7473
Oleksiy Nikolayenko <mennekis@gmail.com>
2024-04-02 23:19:21 +0300

    Started and repeat-1

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
 
 const config = require("config");
+const fs = require("fs");
+const path = require("path");
+
+fs.writeFileSync("myFile.txt", "My data for writing");
 
 const colors = require("colors");
 
 colors.enable();
 
 console.log(colors.red("# started our file"));
-console.info(colors.bgWhite.green(config.db.name));
+console.info(colors.bgWhite.green(config));
 // !console.log(process.env);
+
+const data = fs.readFileSync("myFile.txt", "utf8");
+console.log(data);
