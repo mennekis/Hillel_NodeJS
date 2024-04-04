7e6a7efbbe0eac4b9bda55120db2737bced8f4e4
Oleksiy Nikolayenko <mennekis@gmail.com>
2024-04-04 10:03:42 +0300

    added yup validator

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 
+const yup = require("yup");
+
 router.get("/", (req, resp) => {
    resp.send("#Post is posted");
 });
@@ -9,8 +11,25 @@ router.get("/:postId", (req, resp) => {
 router.get("/:postId/:commentsId", (req, resp) => {
    resp.send("#Post_ID is gotten with COMMENT");
 });
+//     "title": " My Title Post",
+//     "description": " Some text",
+//     "Img": " Image"
+router.post("/", async (req, resp) => {
+   const { body } = req;
 
-router.post("/", (req, resp) => {
+   const metaDataPostSchema = yup.object({
+      title: yup.string().required().min(1),
+      description: yup.string().required().min(1),
+      Img: yup.string().url(),
+   });
+
+   try {
+      const data = await metaDataPostSchema.validate(body);
+   } catch (error) {
+      resp.send(400, error.message);
+      return;
+   }
+
    resp.send(400, "!!! alert ,add req body");
    console.log(req.body);
 });
